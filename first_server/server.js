a50ce8c5629a4c896e4300125c19dca55ec9cfaf
MarioAGR <marioagr95@outlook.com>
2017-07-01 02:52:38 -0600

    Used http library (first_server)

--- first_server/server.js
+++ first_server/server.js
@@ -1,2 +1,13 @@
-var http = require('http');
+var http = require('http'); //To include the http package, all http API's available to the variable
 
+var hostname = '127.0.0.1';
+var port = '8080';
+
+var server = http.createServer(function(req, res){
+    //The method createServer takes a request and response
+    res.end("Helllo from our Node Server");
+});
+
+server.listen(port, hostname, function(){
+    console.log('The server is running');
+});
