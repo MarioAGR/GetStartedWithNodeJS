d653e59d44b05a9c3ae959f9cc902bd6a200f739
MarioAGR <marioagr95@outlook.com>
2017-07-01 04:48:40 -0600

    Fixed somethings, added missing content to app.js

--- app/controllers/api.js
+++ app/controllers/api.js
@@ -3,13 +3,13 @@
 var db = require('../util/db');
 var client = require('../util/twitter');
 
-exports.results = function(req, res) {
+exports.results = function(req, res){
     var query = req.query.q;
     if(query){
         db.getCollection('searches').insert({term: query});
         db.saveDatabase();
 
-        client.get('search/tweets', {q:query}, function(erros, tweets, response) {
+        client.get('search/tweets', {q:query}, function(error, tweets, response){
             res.json(tweets);
         })
     } else {
