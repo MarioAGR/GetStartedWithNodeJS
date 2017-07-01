c7ad0f0b7aec9782de721aa84c1eda3f7f78f9b2
MarioAGR <marioagr95@outlook.com>
2017-07-01 03:22:12 -0600

    Setting up Express

--- app/server.js
+++ app/server.js
@@ -1,1 +1,14 @@
 // Express server config, database initialization and other settings
+
+var express = require('express');
+
+var app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/../public/views');
+
+// Tell where static files are, called for every request
+app.use(express.static(__dirname + '/../public'));
+
+//Export a new app server, any file that imports server.js will have access to the instance of our app
+module.exports = app;
