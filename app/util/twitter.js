b7d93969e33de4f90c860ebf23fedc3e18666682
MarioAGR <marioagr95@outlook.com>
2017-07-01 04:08:21 -0600

    Connecting Twitter API

--- app/util/twitter.js
+++ app/util/twitter.js
@@ -1,1 +1,12 @@
 // Interact with Twitter API
+
+var twitter = require('twitter');
+
+var client = new twitter({
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token_key: process.env.ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET
+});
+
+module.exports = client;
